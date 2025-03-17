4de1379b3b1704c4897e97e8962e4548a74e44f9
Rickiard <al79194@alunos.utad.pt>
2025-03-17 20:46:58 +0000

    Adicionar classe Wavy para comunicação TCP
    Implementa a classe `Wavy` que se conecta a um servidor local na porta 5000. A classe envia a mensagem "WAVY ID 001" e exibe a resposta do servidor no console. Inclui as importações necessárias para `TcpClient` e `Encoding`.

--- WavyClient/Program.cs
+++ WavyClient/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+using System.Text;
+
+class Wavy
+{
+    static void Main()
+    {
+        TcpClient client = new TcpClient("127.0.0.1", 5000);
+        NetworkStream stream = client.GetStream();
+        byte[] message = Encoding.UTF8.GetBytes("WAVY ID 001");
+        stream.Write(message, 0, message.Length);
+
+        byte[] buffer = new byte[1024];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        Console.WriteLine("Servidor respondeu: " + Encoding.UTF8.GetString(buffer, 0, bytesRead));
+    }
+}
